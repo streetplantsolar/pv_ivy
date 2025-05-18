e09539960a7ea7807bd50fe6b1db6004cc6ee957
streetplantsolar <132174038+streetplantsolar@users.noreply.github.com>
2025-05-18 16:39:12 -0700

    Update manage.py

--- backend/manage.py
+++ backend/manage.py
@@ -2,14 +2,12 @@ import os
 import sys
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.config.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            "Couldn't import Django. Are you sure it's installed and available on your PYTHONPATH?"
         ) from exc
     execute_from_command_line(sys.argv)
 
