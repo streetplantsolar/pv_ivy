e727548a272ebedb5217c84a1f995638aa57ddb8
streetplantsolar <132174038+streetplantsolar@users.noreply.github.com>
2025-05-18 19:18:57 -0700

    Update celery.py

--- backend/config/celery.py
+++ backend/config/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.config.settings')
 
 app = Celery('pv_ivy')
 app.config_from_object('django.conf:settings', namespace='CELERY')
