23778824c5d84ab59bd510133099895f3ad28b5d
streetplantsolar <132174038+streetplantsolar@users.noreply.github.com>
2025-05-18 20:41:47 -0700

    Create Diagnostics.js

---
+++ frontend/src/pages/Diagnostics.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Diagnostics = () => {
+  return (
+    <div>
+      <h2>Diagnostics Page</h2>
+      <p>This is the Diagnostics page content.</p>
+    </div>
+  );
+};
+
+export default Diagnostics;
