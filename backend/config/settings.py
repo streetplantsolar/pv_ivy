cc081cb4450be445b46f79c8dcd80b2a2198e4b7
streetplantsolar <132174038+streetplantsolar@users.noreply.github.com>
2025-05-18 19:21:18 -0700

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -39,7 +39,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'config.urls'
+ROOT_URLCONF = 'backend.config.urls'
 
 TEMPLATES = [
     {
@@ -57,7 +57,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'config.wsgi.application'
+WSGI_APPLICATION = 'backend.config.wsgi.application'
 
 # Database
 DATABASES = {
