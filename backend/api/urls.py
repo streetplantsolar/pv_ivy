6e30b78a6adef6f8cd9b02c82b5d54b86cc85483
streetplantsolar <132174038+streetplantsolar@users.noreply.github.com>
2025-05-19 07:56:02 -0700

    Update urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 
 urlpatterns = [
-    path('auth/register/', views.RegisterView.as_view(), name='register'),
+    path('', TemplateView.as_view(template_name="home/landing_page.html"), name='landing-page'),
+    path('auth/register/', TemplateView.as_view(template_name="home/landing_page.html"), name='signup'),  # Reuse the same template,
     path('auth/login/', obtain_auth_token, name='login'),
     path('iv-data/upload/', views.IVDataUploadView.as_view(), name='iv_data_upload'),
     path('diagnostics/', views.DiagnosticsView.as_view(), name='diagnostics')
